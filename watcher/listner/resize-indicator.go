f33280fe35bf65668e5e05039b6d20ddea615c8b
Mariano Punzo <mcpunzo@gmail.com>
2026-06-18 23:17:41 +0200

    feat: add resize event statuses tracking
    Signed-off-by: Mariano Punzo <mcpunzo@gmail.com>

--- watcher/listner/resize-indicator.go
+++ watcher/listner/resize-indicator.go
@@ -13,7 +13,10 @@ type ResizeIndicator struct {
 	NumberOfRecommendations int
 	// RecommendationProcessed is the number of recommendations that have been processed so far.
 	RecommendationProcessed int
-	lock                    *sync.RWMutex
+	// StatusMap is a map that keeps track of the count of each resize status.
+	StatusMap map[watcher.ResizeStatus]int
+
+	lock *sync.RWMutex
 }
 
 // NewResizeIndicator creates a new instance of ResizeIndicator with the specified number of recommendations.
@@ -23,15 +26,24 @@ func NewResizeIndicator(numberOfRecommendations int) *ResizeIndicator {
 	return &ResizeIndicator{
 		NumberOfRecommendations: numberOfRecommendations,
 		RecommendationProcessed: 0,
+		StatusMap:               make(map[watcher.ResizeStatus]int),
 		lock:                    &sync.RWMutex{},
 	}
 }
 
+// HandleResizeEvent is a method that handles resize events and updates the count of processed recommendations accordingly.
+// It also updates the status map with the count of each resize status and logs the progress of processing recommendations.
+// param event The resize event that contains information about the recommendations processed and their status.
 func (r *ResizeIndicator) HandleResizeEvent(event *watcher.ResizeEvent) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 
 	r.RecommendationProcessed += len(event.Recommendation)
+	r.StatusMap[event.Status] += len(event.Recommendation)
 
 	log.Info().Msgf("ResizeIndicator: Processed %d/%d recommendations", r.RecommendationProcessed, r.NumberOfRecommendations)
+
+	log.Info().Msgf("%s/%d, %s/%d, %s/%d, %s/%d, %s/%d", watcher.ResizeSucceeded, r.StatusMap[watcher.ResizeSucceeded],
+		watcher.ResizeFailed, r.StatusMap[watcher.ResizeFailed], watcher.ResizeRollbackSucceeded, r.StatusMap[watcher.ResizeRollbackSucceeded], watcher.ResizeRollbackFailed, r.StatusMap[watcher.ResizeRollbackFailed], watcher.ResizeSkipped, r.StatusMap[watcher.ResizeSkipped])
+
 }
